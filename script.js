66843967ef08dcca0f6b320e3ef62fee6b50d749
sumaiyatheauthor <talberttionne@gmail.com>
2025-07-19 14:17:23 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+<div id="popup-form" class="newsletter-popup">
+  <div class="popup-content">
+    <h3>💌 Join My Newsletter</h3>
+    <p>Get exclusive poems, behind-the-scenes thoughts, and early book news.</p>
+    <form action="https://yournewsletterurl.com" method="POST" target="_blank">
+      <input type="email" name="email" placeholder="Enter your email" required />
+      <button type="submit">Subscribe</button>
+    </form>
+    <span id="close-btn">✖</span>
+  </div>
+</div>
